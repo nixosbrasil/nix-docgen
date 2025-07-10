fcfb491bcfb1cd190b5b3cd8bda03010327efee3
lucasew <lucas59356@gmail.com>
2025-07-10 15:20:33 -0300

    tweaking
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import html
+import json
+import time
 import jinja2
 import subprocess
 from datetime import datetime
@@ -13,12 +15,15 @@ from shutil import which
 import logging
 import sys
 import shlex
+import time
+import bs4
 
 logger = logging.getLogger("nix-docgen")
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
 IS_VERBOSE = False
+BASE_URL = "http://localhost:1313"
 
 
 def norm_branch(branch):
@@ -55,29 +60,23 @@ def build_rev_attribute(rev, attribute):
     assert ret.exists()
     return ret
 
+def render_template(template, args={}):
+    args['baseurl'] = BASE_URL
+    args['timestamp'] = int(time.time())
+    loader = jinja2.FileSystemLoader(str(SCRIPT_DIR))
+    env = jinja2.Environment(loader=loader)
+    template = env.get_template(template)
+    return template.render(args)
 
 def render_index_html(context):
-    """
-    Renders the index.html.jinja template with the provided context.
-    :param context: dict with variables for the template (e.g.: generated_at, branches)
-    :return: string with the rendered HTML
-    """
-    loader = jinja2.FileSystemLoader(str(SCRIPT_DIR))
-    env = jinja2.Environment(loader=loader)
-    template = env.get_template('index.html.jinja')
-    return template.render(context)
+    return render_template('index.html.jinja', context)
 
+
+def render_docset_xml(branch):
+    return render_template('docset.xml.jinja', {'branch': branch})
 
 def render_info_plist(rev):
-    """
-    Renders the Info.plist.jinja template with only the rev parameter.
-    :param rev: string with the value for the rev field
-    :return: string with the rendered Info.plist
-    """
-    loader = jinja2.FileSystemLoader(str(SCRIPT_DIR))
-    env = jinja2.Environment(loader=loader)
-    template = env.get_template('Info.plist.jinja')
-    return template.render(rev=rev)
+    return render_template('Info.plist.jinja', {'rev': rev})
 
 
 def process_branch_list(branches):
@@ -96,80 +95,31 @@ def assert_equal(a, b):
 assert_equal(process_branch_list(["a", "b"]), {"a", "b"})
 assert_equal('stable' not in process_branch_list(["a", "stable"]), True)
 
-def generate_zeal(branch: str, nixpkgs: Path, output_file: Path):
+def generate_zeal(branch: str, output_file: Path):
     """
     Generates a Zeal/Dash docset from the extracted nixpkgs tree, packaging everything into output_file (.tgz).
     """
-    def remove_ansi_escape_codes(text):
-        ansi_escape = re.compile(r'''
-        \x1B  # ESC
-        (?:   # 7-bit C1 Fe (except CSI)
-            [@-Z\\-_]
-        |     # or [ for CSI, followed by a control sequence
-            \[
-            [0-?]*  # Parameter bytes
-            [ -/]*  # Intermediate bytes
-            [@-~]   # Final byte
-        )
-        ''', re.VERBOSE)
-        return ansi_escape.sub('', text)
 
-    def get_lib_sections(nixpkgs_path):
-        lib_function_docs = nixpkgs_path / "doc" / "doc-support" / "lib-function-docs.nix"
-        ret = []
-        with open(str(lib_function_docs), 'r') as f:
-            while True:
-                line = f.readline()
-                if len(line) == 0:
-                    break
-                line = line.strip()
-                if line.startswith("docgen"):
-                    _, key, *value = line.split(' ')
-                    value = " ".join(value).strip("'")
-                    ret.append((key, value))
-        return ret
-
-    def register_section(DB, OBJECTS, key, value, kind="Property"):
+    def register_section(DB, key, value, kind="Property"):
         key = key.strip()
         value = value.strip()
         OBJECTS[key] = value
         DB.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) values (?, ?, ?);', (key, kind, f"#{key}"))
 
-    def ingest_lib_documentation(DB, OBJECTS, nixpkgs_path, base="lib", filename="default.nix"):
-        lines = run_subprocess([
-            "nix", "run", "nixpkgs#nix-doc", "--",
-            "search", ".*", str(nixpkgs_path / "lib" / filename)
-        ], capture=True, text=True).split('\n')
-        lines = iter(lines)
-        curdoc = ""
-        funcname = None
-        definedAt = None
-        while True:
-            try:
-                line = next(lines)
-                if line.startswith('\x1b[38;5;15;1m'):
-                    line = remove_ansi_escape_codes(line)
-                    funcname = line.split('=')[0]
-                    curdoc = f'{line} \n{curdoc}'
-                    continue
-                if line.startswith('# /nix'):
-                    line = line[2:]
-                    definedAt = line
-                    register_section(DB, OBJECTS,
-                        f'{base}.{funcname}',
-                        f'{curdoc}\nDefined at: {definedAt}',
-                        "Function"
-                    )
-                    curdoc = ""
-                    funcname = None
-                    definedAt = None
-                    next(lines)
-                    continue
-                curdoc = f'{curdoc}\n{line}'
-            except StopIteration:
-                break
 
-    def generate_zeal_index(nixpkgs_path: Path, output_html: Path, index_db: Path, branch: str = "master"):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        docset_root = Path(tmpdir) / 'nixpkgs.docset'
+        docset_root.mkdir(parents=True, exist_ok=True)
+        (docset_root / "meta.json").write_text(json.dumps({
+            "name": "Nixpkgs",
+            "revision": branch,
+            "title": "Nixpkgs"
+        }))
+        contents = docset_root / 'Contents'
+        resources = contents / 'Resources'
+        documents = resources / 'Documents'
+        documents.mkdir(parents=True, exist_ok=True)
+        index_db = resources / 'docSet.dsidx'
         conn = sqlite3.connect(index_db)
         DB = conn.cursor()
         OBJECTS = {}
@@ -180,37 +130,22 @@ def generate_zeal(branch: str, nixpkgs: Path, output_file: Path):
         DB.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
         DB.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
-        for (section, description) in get_lib_sections(nixpkgs_path):
-            register_section(DB, OBJECTS, "lib." + section, description, "Environment")
-            ingest_lib_documentation(DB, OBJECTS, nixpkgs_path, base=f'lib.{section}', filename=f'{section}.nix')
+        NIXOS_INDEX_FILE = output_file.parent / "nixos" / "index.html"
+        NIXOS_INDEX = bs4.BeautifulSoup(NIXOS_INDEX_FILE.read_text())
+        for item in NIXOS_INDEX.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+            if 'id' not in item:
+                continue
+            item_id = item['id']
+            register_section(DB, item.text, str(NIXOS_INDEX_FILE.relative_to(output_file.parent)) + "#" + str(item_id), kind='Section')
 
+        # TODO: generate index
         conn.commit()
         conn.close()
-
-        keys = list(OBJECTS.keys())
-        keys.sort()
-        html_content = ""
-        for key in keys:
-            html_content += f'<h1 id="{key}">{key}</h1>'
-            html_content += f"<pre>{html.escape(OBJECTS[key])}</pre>\n"
-        output_html.write_text(html_content, encoding='utf-8')
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        docset_root = Path(tmpdir) / 'nixpkgs.docset'
-        contents = docset_root / 'Contents'
-        resources = contents / 'Resources'
-        documents = resources / 'Documents'
-        documents.mkdir(parents=True, exist_ok=True)
-        index_db = resources / 'docSet.dsidx'
-        output_html = documents / 'index.html'
-        generate_zeal_index(
-            nixpkgs_path=nixpkgs,
-            output_html=output_html,
-            index_db=index_db,
-            branch=branch
-        )
         (contents / 'Info.plist').write_text(render_info_plist(branch), encoding='utf-8')
         with tarfile.open(output_file, 'w:gz') as tar:
+            shutil.copytree(output_file.parent / "nixpkgs", documents/"nixpkgs", symlinks=True)
+            shutil.copytree(output_file.parent / "nixos", documents/"nixos", symlinks=True)
+            (output_file / "index.html").write_text("<h1>Foi</h1>")
             tar.add(docset_root, arcname='nixpkgs.docset')
 
 def build_branches(branches):
@@ -234,12 +169,9 @@ def build_branches(branches):
 
         nixpkgs_docs = build_rev_attribute(branch, "htmlDocs.nixpkgsManual.x86_64-linux")
         nixos_docs = build_rev_attribute(branch, "htmlDocs.nixosManual.x86_64-linux")
-        nixpkgs = build_rev_attribute(branch, "path")
         branch_target = target_dir / norm
         branch_target.mkdir(parents=True, exist_ok=True)
-        generate_zeal(
-            branch, nixpkgs, branch_target / "nixpkgs.docset.tgz"
-        )
+        (branch_target / "docset.xml").write_text(render_docset_xml(branch))
         for source in [nixpkgs_docs, nixos_docs]:
             doc_dir = (source / "share" / "doc")
             for item in doc_dir.glob('**/*'):
@@ -248,10 +180,14 @@ def build_branches(branches):
                 destination = branch_target / item.relative_to(doc_dir)
                 destination.parent.mkdir(parents=True, exist_ok=True)
                 destination.symlink_to(item)
+        generate_zeal(
+            branch, branch_target / "nixpkgs.docset.tgz"
+        )
 
 
 def main():
     global IS_VERBOSE
+    global BASE_URL
     parser = argparse.ArgumentParser(description="Generates documentation and docsets for multiple nixpkgs branches.")
     parser.add_argument(
         "branches",
@@ -263,8 +199,14 @@ def main():
         action="store_true",
         help="Enable detailed logging (DEBUG)"
     )
+    parser.add_argument(
+        "--base-url", "-b",
+        default=BASE_URL,
+        help="Base URL for links"
+    )
     args = parser.parse_args()
     IS_VERBOSE = args.verbose
+    BASE_URL = args.base_url
     logging.basicConfig(
         level=logging.DEBUG if args.verbose else logging.INFO,
         format='[%(levelname)s] %(message)s'
